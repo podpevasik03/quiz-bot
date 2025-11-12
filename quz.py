d46036876b7c7a880eadeb62624f67bd09ef5d56
matvei <podpevasik03@gmail.com>
2025-11-12 19:40:13 +0300

    заполнен main.py

--- quz.py
+++ quz.py
@@ -1,1 +1,8 @@
 # Система вопросов и ответов
+import json
+
+def get_question():
+    with open("questions.json", "r", encoding="utf-8") as f:
+        questions = json.load(f)
+    return questions[0]  # пока берём первый, можно заменить на случайный
+
